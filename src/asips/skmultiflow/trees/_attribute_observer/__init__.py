91e6e1f7024cf8657796e64203d50c4f8429cf49
Venoli <venolishehara@gmail.com>
2021-09-26 15:05:19 +0530

    Setup Asips

---
+++ src/asips/skmultiflow/trees/_attribute_observer/__init__.py
@@ -0,0 +1,13 @@
+from .attribute_observer import AttributeObserver
+from .attribute_observer_null import AttributeObserverNull
+from .nominal_attribute_class_observer import NominalAttributeClassObserver
+from .nominal_attribute_regression_observer import NominalAttributeRegressionObserver
+from .numeric_attribute_class_observer_binary_tree import NumericAttributeClassObserverBinaryTree
+from .numeric_attribute_class_observer_gaussian import NumericAttributeClassObserverGaussian
+from .numeric_attribute_regression_observer import NumericAttributeRegressionObserver
+from .numeric_attribute_class_observer_gaussian_hellinger import NumericAttributeClassObserverGaussianHellinger
+
+__all__ = ["AttributeObserver", "AttributeObserverNull", "NominalAttributeClassObserver",
+           "NominalAttributeRegressionObserver", "NumericAttributeClassObserverBinaryTree",
+           "NumericAttributeClassObserverGaussian", "NumericAttributeClassObserverGaussianHellinger",
+           "NumericAttributeRegressionObserver"]
---
+++ src/asips/skmultiflow/trees/_split_criterion/__init__.py
@@ -0,0 +1,11 @@
+from .base_split_criterion import SplitCriterion
+from .gini_split_criterion import GiniSplitCriterion
+from .hellinger_distance_criterion import HellingerDistanceCriterion
+from .info_gain_split_criterion import InfoGainSplitCriterion
+from .variance_reduction_split_criterion import VarianceReductionSplitCriterion
+from .intra_cluster_variance_reduction_split_criterion import \
+    IntraClusterVarianceReductionSplitCriterion
+from .gaussian_hellinger_distance_criterion import GaussianHellingerDistanceCriterion
+__all__ = ["SplitCriterion", "GaussianHellingerDistanceCriterion", "GiniSplitCriterion", "HellingerDistanceCriterion",
+           "InfoGainSplitCriterion", "IntraClusterVarianceReductionSplitCriterion",
+           "VarianceReductionSplitCriterion"]
